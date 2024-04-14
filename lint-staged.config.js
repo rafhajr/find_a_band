b81488667ed26f9467b29f540514716bb8fce877
Rafhael Souza <rafhael.c.junior@gmail.com>
2024-04-14 17:57:15 -0300

    chore: add husky to project

---
+++ lint-staged.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  '*.{js,jsx,ts,tsx}': 'eslint --fix'
+  // '*.{ts,tsx,css}': ['prettier . --write']
+  // '*.{ts,tsx}': ['eslint . --cache --fix --ext .tsx --ext .ts', () => 'yarn tsc', () => 'yarn vitest run']
+};
