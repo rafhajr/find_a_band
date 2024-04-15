cbf5d7c3e36826e40818e3b2d51841c6f472542e
Rafhael Souza <rafhael.c.junior@gmail.com>
2024-04-15 01:14:19 -0300

    feature: create header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import { Title } from '../Typography';
+
+export const Header = () => {
+  return (
+    <div className="flex w-full bg-primary-500 p-5">
+      <Title>Find a Band</Title>
+    </div>
+  );
+};
