6f01446654dad7de66dcff4729014f001aca1171
Rafhael Souza <rafhael.c.junior@gmail.com>
2024-04-15 23:33:07 -0300

    feature: improve instrument page

--- src/app/instrument/[instrument]/page.tsx
+++ src/app/instrument/[instrument]/page.tsx
@@ -1,15 +1,39 @@
-import { CardTitle, Header, MusicianPreview } from '@/components';
+'use client';
 
-const Home = () => {
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+import { getInstrumentPageInfo, validInstruments } from '../utils';
+
+import { Header, MusicianPreview, Title } from '@/components';
+
+const InstrumentPage = ({ params }: { params: { instrument: string } }) => {
+  const router = useRouter();
+
+  if (!validInstruments.includes(params.instrument.toLowerCase())) {
+    return router.push('/');
+  }
+
+  const { image, title, width } = getInstrumentPageInfo('');
+
   return (
     <div>
       <Header />
-
-      <div className="flex flex-col gap-4 px-20 py-10 w-full rounded-2xl">
-        <div className="flex items-center justify-between pb-2 border-b-1 border-default-600">
-          <CardTitle className="text-default-600">Instrument</CardTitle>
+      <div className="flex flex-col bg-default-700 items-center h-[500px] gap-8 px-12 py-12">
+        <div className="flex items-center justify-evenly w-full">
+          <div className="flex flex-col gap-2">
+            <Title>{title}</Title>
+          </div>
+          <div className="flex">
+            <Image
+              src={image}
+              alt="band"
+              width={width}
+              className="bg-primary-500 rounded-ss-[60px] rounded-ee-[60px]"
+            />
+          </div>
         </div>
-        <div className="flex gap-5 pt-10">
+        <div className="flex gap-5 mt-5">
           <MusicianPreview />
           <MusicianPreview />
           <MusicianPreview />
@@ -20,4 +44,4 @@ const Home = () => {
   );
 };
 
-export { Home as default };
+export { InstrumentPage as default };
