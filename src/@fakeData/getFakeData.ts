3ddbed28a285d5a67af8ef51c09631b8fd0b5881
Rafhael Souza <rafhael.c.junior@gmail.com>
2024-05-03 19:21:29 -0300

    chore: update get fake data

---
+++ src/@fakeData/getFakeData.ts
@@ -0,0 +1,9 @@
+import fakeData from '@/@fakeData/fakeData.json';
+
+export const allMusicians = [
+  { instrument: 'Bass', musicians: fakeData.bassPlayers },
+  { instrument: 'Drums', musicians: fakeData.drummers },
+  { instrument: 'Guitar', musicians: fakeData.guitarPlayers },
+  { instrument: 'Keyboard', musicians: fakeData.keyboardPlayers },
+  { instrument: 'Singer', musicians: fakeData.singers }
+];
